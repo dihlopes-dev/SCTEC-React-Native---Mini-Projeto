7ff5e1e322267742c139837cb7cbb8178db5e8cc
dihlopes-dev <diegolopes.oab@gmail.com>
2026-08-01 12:45:49 -0300

    Create cinematch.js

---
+++ CineMatch-JS/cinematch.js
@@ -0,0 +1,56 @@
+//==========================================================================
+//***SCTEC: Projeto Avaliativo M1S6 - Desenvolvimento Mobile React Native***
+//==========================================================================
+
+//***Onboarding Interativo ( Via Terminal )
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+// ==========================================
+// RF01 — Captura de Perfil via Terminal
+// ==========================================
+function criarPerfilUsuario() {
+  console.log("--- BEM-VINDO AO CINEMATCH JS ---");
+  const nome = prompt("Qual é o seu nome? ");
+  const idade = Number(prompt("Qual é a sua idade? "));
+  const generosInput = prompt("Quais gêneros você mais gosta? (separe por vírgula, ex: Ação, Comédia, Terror): ");
+
+  const generosFavoritos = [];
+  let generoAtual = "";
+
+  for (let i = 0; i < generosInput.length; i++) {
+    const caractere = generosInput[i];
+
+    if (caractere === ',') {
+      
+      const generoLimpo = generoAtual.trim();
+      if (generoLimpo !== "") {
+        generosFavoritos.push(generoLimpo);
+      }
+      generoAtual = "";
+    } else {
+      generoAtual += caractere;
+    }
+  }
+
+  const ultimoGenero = generoAtual.trim();
+  if (ultimoGenero !== "") {
+    generosFavoritos.push(ultimoGenero);
+  }
+
+  const usuario = {
+    nome: nome || "Convidado",
+    idade: idade || 0,
+    generosFavoritos: generosFavoritos
+  };
+
+  return usuario;
+}
+
+//Teste do código no terminal
+
+const usuarioAtual = criarPerfilUsuario()
+
+console.log ("--- Usuário Criado com Sucesso! ---")
+console.log (usuarioAtual)
+
